1dc5541aff23f3fbff2b3e62b2a31a374d57f0a2
Jim O'Halloran <jim@tuxit.com.au>
2013-11-23 12:29:32 +1030

    Mix UDP and config code together

--- controller/controller.js
+++ controller/controller.js
@@ -1,6 +1,26 @@
-require('../common/config.js');
-
+var Config = require('../common/config.js');
 Config.init(function() {
+
+	// Original source for UDP listener code:
+	//   http://www.robertprice.co.uk/robblog/2011/03/writing_a_udp_server_using_node_js-shtml/
+
+	var dgram = require("dgram");
+
+	var server = dgram.createSocket("udp4");
+
+	server.on("message", function (msg, rinfo) {
+		console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+	});
+
+	server.on("listening", function () {
+		var address = server.address();
+		console.log("server listening " + address.address + ":" + address.port);
+	});
+
+	server.on("error", function (exception) {
+		console.log("Exception: " + exception);
+	});
+	server.bind(43278, '127.0.0.1');
 	
 });
 
