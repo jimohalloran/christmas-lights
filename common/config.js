3faa2bc94ae3210a650d6c9367f7ef66f23a98b9
Jim O'Halloran <jim@tuxit.com.au>
2013-11-23 12:29:01 +1030

    Exported config class as a propper node module

--- common/config.js
+++ common/config.js
@@ -1,70 +1,68 @@
 
-var Config = {
-
-	_fs: require('fs'),
-	_file: __dirname + '/../master.json',
+var	_fs = require('fs');
+var _file = __dirname + '/../master.json';
 	
-	controllers: {},
-	lights: {},
-	channelMap: {},
-	
-	init: function(cb) {
-		this._fs.readFile(this._file, 'utf8', function (err, data) { 
-			if (err) {
-				console.log('Error: ' + err);
-				return; 
-			}
+exports.controllers = {};
+exports.lights = {};
+exports.channelMap = {};
 
-			var master = JSON.parse(data);
-			Config.controllers = master.controllers;
-			Config.lights = master.lights;
-			Config._buildChannelMap();
-			Config._validateLights();
-			cb();
-		});
-	},
+function _buildChannelMap() {
+	Object.keys(exports.controllers).forEach(function(nodeId) { 
+		var node = exports.controllers[nodeId];
+		switch (node.type) {
+			case "arduinotlc":
+				node.channels.forEach(function(channelName, channelNum) {
+					exports.channelMap[channelName] = {
+						"node": nodeId, 
+						"channel": channelNum
+					};
+				});
+				break;
+		}
+	});
+}
 
-	_buildChannelMap: function() {
-		Object.keys(this.controllers).forEach(function(nodeId) { 
-			var node = Config.controllers[nodeId];
-			switch (node.type) {
-				case "arduinotlc":
-					node.channels.forEach(function(channelName, channelNum) {
-						Config.channelMap[channelName] = {
-							"node": nodeId, 
-							"channel": channelNum
-						};
-					});
-					break;
-			}
-		});
-	},
-
-	_validateLights: function() {
-		isLightConfigValid = true;
-		Object.keys(this.lights).forEach(function(lightId) {
-			var light = Config.lights[lightId];
-			switch (light.type) {
-				case "rgb":
-					light.channels.forEach(function(channelName, idx) {
-						if (Config.channelMap[channelName] == undefined) {
-							console.log("Channel "+channelName+" at index "+idx+" of light "+lightId+" is undefined.");
-							isLightConfigValid = false;
-						}
-					});
-					break;
-				
-				case "mono":
-					if (Config.channelMap[light.channel] == undefined) {
-						console.log("Channel "+light.channel+" of light "+lightId+" is undefined.");
+function _validateLights() {
+	isLightConfigValid = true;
+	Object.keys(exports.lights).forEach(function(lightId) {
+		var light = exports.lights[lightId];
+		switch (light.type) {
+			case "rgb":
+				light.channels.forEach(function(channelName, idx) {
+					if (exports.channelMap[channelName] == undefined) {
+						console.log("Channel "+channelName+" at index "+idx+" of light "+lightId+" is undefined.");
 						isLightConfigValid = false;
 					}
-					break;
-			}
-		});
-
-		if (!isLightConfigValid) {
-			process.exit(1);
+				});
+				break;
+			
+			case "mono":
+				if (exports.channelMap[light.channel] == undefined) {
+					console.log("Channel "+light.channel+" of light "+lightId+" is undefined.");
+					isLightConfigValid = false;
+				}
+				break;
 		}
+	});
+
+	if (!isLightConfigValid) {
+		process.exit(1);
 	}
+}
+
+	
+exports.init = function(cb) {
+	_fs.readFile(_file, 'utf8', function (err, data) { 
+		if (err) {
+			console.log('Error: ' + err);
+			return; 
+		}
+
+		var master = JSON.parse(data);
+		exports.controllers = master.controllers;
+		exports.lights = master.lights;
+		_buildChannelMap();
+		_validateLights();
+		cb();
+	});
 };
